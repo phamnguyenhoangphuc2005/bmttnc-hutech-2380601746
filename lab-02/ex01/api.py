c73d51a981c5fcf11305a88a43a8dcb3bba9a8ee
hoangphuc <phamnguyenhoangphuc2005@gmail.com>
2026-05-22 10:35:41 +0700

    10:35

---
+++ lab-02/ex01/api.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from cipher.caesar import CaesarCipher
+app = Flask(__name__)
+
+#CAESAR CIPHER ALGORITHM
+caesar_cipher = CaesarCipher()
+
+@app.route("/api/ceasar/encrypt", methods=["POST"])
+def caesar_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypt_text = caesar_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_message' : encrypt_text})
+
+@app.route("/api/ceasar/decrypt", methods=["POST"])
+def ceasar_decrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    decrypt_text = caesar_cipher.decrypt_text(plain_text, key)
+    return jsonify({'decrypted_message' : decrypt_text})
+
+#Main function
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
